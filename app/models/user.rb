4ce3a44955bd62d17e7afe96c7e62c7b349d13d3
Ryo-0131 <kankichi01kantoku@gmail.com>
2022-05-05 16:37:16 +0900

    Prefecture_idとfavorite_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :favorite
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 has_many :reviews, dependent: :destroy
 has_many :likes, dependent: :destroy
+
+validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+validates :favorite_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
