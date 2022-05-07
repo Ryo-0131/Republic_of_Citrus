f139fc5d79fbbb74a580bea991860c6b629a3af4
Ryo-0131 <kankichi01kantoku@gmail.com>
2022-05-07 23:11:46 +0900

    ユーザー管理⑦（アクセス権限）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,42 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :prevent_url, only: [:edit, :destroy]
+
   def create
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+   if current_user.update(user_params)
+       redirect_to root_path
+    else
+       render :edit
+    end
+  end
+ 
+   private
+
+   def set_user
     @user = User.find(params[:id]) 
    end
 
-   def edit
+   def prevent_url
+    unless @user.id == current_user.id
+      redirect_to root_path
+    end
    end
- 
-   def update
-    if current_user.update(user_params)
-       redirect_to root_path
-     else
-       render :edit
-     end
-   end
- 
-   private
  
    def user_params
      params.require(:user).permit(:name, :email, :nickname, :prefecture_id, :favorite_id, :introduction, :image)
    end
 
-end
+  end
+
+
+  
