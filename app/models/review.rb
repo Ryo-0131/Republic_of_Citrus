e0b9c05ca28259b1cb670285e3cbecf960ee1a73
Ryo-0131 <kankichi01kantoku@gmail.com>
2022-05-20 23:52:56 +0900

    5段階評価機能①（星評価の表示）

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,4 +10,8 @@ class Review < ApplicationRecord
   validates :rating3, presence: true
   validates :rating4, presence: true
 
+  validates :all_rating, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1}, presence: true
+
 end
