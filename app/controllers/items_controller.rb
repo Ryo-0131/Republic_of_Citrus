cf5bcea3001b70156c4ddcb2c32126821a887b93
Ryo-0131 <kankichi01kantoku@gmail.com>
2022-06-05 23:35:45 +0900

    CSS10_reviewedit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-  
-    # Item.order(all_rating: :desc)
+      # Item.order(all_rating: :desc)
     @reviews = Review.all
     @all_rating = "総合評価"
     @rating1 = "甘味評価"
@@ -13,8 +12,7 @@ class ItemsController < ApplicationController
     @rating4 = "食べやすさ評価"  
     @itemname =  Item.pluck(:item_name)
     gon.itemname = @itemname
- 
-  end
+   end
 
   def show
     @item = Item.find(params[:id])
@@ -34,7 +32,7 @@ class ItemsController < ApplicationController
     @rating3 = "水分評価"
     @rating4 = "食べやすさ評価" 
 
-   gon.allave = @reviews.average(:all_rating)
+    gon.allave = @reviews.average(:all_rating)
  end
 
  def ranking
