a3ca7c636f2ae50af35f7d12d830c7beb9339960
Ryo-0131 <kankichi01kantoku@gmail.com>
2022-06-18 23:31:19 +0900

    Seed.db更新

---
+++ .history/db/seeds_20220618092345.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('db/items.csv') do |row|
+  Item.create(item_name: row[0], description: row[1], image_url: row[2], star: row[3])
+end
